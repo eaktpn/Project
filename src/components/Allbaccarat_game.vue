643739782f57ca23ff2eece095cf12764a1917c7
eaktpn <eaktpn@gmail.com>
2020-05-19 13:59:19 +0700

    .

--- src/components/Allbaccarat_game.vue
+++ src/components/Allbaccarat_game.vue
@@ -1,15 +1,36 @@
 <template>
-   <div>
-      <div class="BG-linear">
-         <div class="container">
-            <div class="row">
-               <div><img src="/logo/aba_spark.png" class="mx-2" width="100%;" style="max-width:125px;" /></div>
-               <div class="col align-self-center text-right">
-                  <div class="font20" style="font-weight:400; margin-top:-30px;">
-                     <span class="mx-1"><img src="/icon/user.png" class="m-2" width="100%;" style="max-width:30px;" />ADO</span>
-                     <span class="mr-2"><img src="/icon/coin.png" class="rotate-y-positive mx-2" width="100%;" style="max-width:30px;" />200.00</span>
-                  </div>
-                  <!-- <div class="row" style="margin-top:-5px;">
+  <div>
+    <div class="BG-linear">
+      <div class="container">
+        <div class="row">
+          <div>
+            <img
+              src="/logo/baccarat.png"
+              class="mx-2"
+              width="100%;"
+              style="max-width:125px;"
+            />
+          </div>
+          <div class="col align-self-center text-right">
+            <div class="font20" style="font-weight:400; margin-top:-30px;">
+              <span class="mx-1"
+                ><img
+                  src="/icon/user.png"
+                  class="m-2"
+                  width="100%;"
+                  style="max-width:30px;"
+                />ADO</span
+              >
+              <span class="mr-2"
+                ><img
+                  src="/icon/coin.png"
+                  class="rotate-y-positive mx-2"
+                  width="100%;"
+                  style="max-width:30px;"
+                />200.00</span
+              >
+            </div>
+            <!-- <div class="row" style="margin-top:-5px;">
                   <div class="col align-self-center">
                      <router-link to="/"><img src="/logo/sexybaccarat.png" width="100%;" style="max-width:100px;"/></router-link>
                   </div>
@@ -26,82 +47,162 @@
                      <router-link to="/"><img src="/logo/sagaming.png" width="100%;" style="max-width:100px;"/></router-link>
                   </div>
                </div> -->
-               </div>
-            </div>
-            <div class="linear_gradient ml-3" style="margin-top:-37px;"></div>
-            <div class="row mt-4">
-               <div class="col"><img src="/banner/banner.png" width="100%;" /></div>
-            </div>
-            <div class="row justify-content-center mt-2">
-               <div class="col-xl col-md col-4"><img src="/btn_game/sexybaccarat.png" class="animate-flicker" width="100%;" /></div>
-               <div class="col-xl col-md col-4"><img src="/btn_game/dreamgaming.png" width="100%;" /></div>
-               <div class="col-xl col-md col-4"><img src="/btn_game/pretty.png" class="animate-flicker" width="100%;" /></div>
-               <div class="col-xl col-md col-4"><img src="/btn_game/ag.png" width="100%;" /></div>
-               <div class="col-xl col-md col-4"><img src="/btn_game/sa.png" class="animate-flicker" width="100%;" /></div>
-            </div>
-         </div>
+          </div>
+        </div>
+        <div
+          class="linear_gradient"
+          style="margin: -35px 0px 0px 100px;"
+        ></div>
+        <div class="row mt-4">
+          <div class="col"><img src="/banner/banner.png" width="100%;" /></div>
+        </div>
+        <div class="row justify-content-center mt-2">
+          <div class="col-xl col-md col-4">
+            <router-link to="/"
+              ><img
+                src="/btn_game/sexybaccarat.png"
+                class="animate-flicker"
+                width="100%;"
+            /></router-link>
+          </div>
+          <div class="col-xl col-md col-4">
+            <router-link to="/"
+              ><img src="/btn_game/dreamgaming.png" width="100%;"
+            /></router-link>
+          </div>
+          <div class="col-xl col-md col-4">
+            <router-link to="/"
+              ><img
+                src="/btn_game/pretty.png"
+                class="animate-flicker"
+                width="100%;"
+            /></router-link>
+          </div>
+          <div class="col-xl col-md col-4">
+            <router-link to="/"
+              ><img src="/btn_game/ag.png" width="100%;"
+            /></router-link>
+          </div>
+          <div class="col-xl col-md col-4">
+            <router-link to="/"
+              ><img src="/btn_game/sa.png" class="animate-flicker" width="100%;"
+            /></router-link>
+          </div>
+        </div>
       </div>
-      <div class="container my-4">
-         <div class="module-border-wrap-condition">
-            <div class="module-border-condition">
-               <div class="row justify-content-center">
-                  <div class="col-xl-12 col-md-12 col-12 text-center">
-                     <div class="color_orange font22" style="font-weight:500;">ALLBACCARAT</div>
-                     <div class="font22" style="font-weight:400;">บาคาร่าออนไลน์</div>
-                  </div>
-                  <div class="col-xl-5 col-md-5 col-12"><img src="/sexy.png" width="100%;" /></div>
-                  <div class="col-xl-7 col-md-7 col-12 font14 mt-3">เราเป็นผู้ให้บริการบาคาร่าออนไลน์แบบเรียลไทม์ ส่งตรงจากบ่อน Casino ผู้เล่นสามารถดูไพ่ที่แจก ก่อนเริ่มเดิมพันได้ในเวลา 20-30 วินาที Baccarat Online เป็นเกมส์ที่มีเสน่ห์ มีนักพนันเล่นกันเป็นจำนวนมาก ผู้เล่นยังสามารถรับเงินรางวัลจากเกมส์ไปได้ ภายในเวลาไม่กี่วินาทีก่อนที่เกมส์การแข่งขันจะจบลงอีกด้วย และผู้เล่นจะได้รับประสบการณ์ การเล่นคาสิโนออนไลน์ที่ปลอดภัย โดยมีระบบตรวจสอบที่ได้รับการยอมรับในระดับสากล สมัครเล่นบาคาร่ากับ Sexybaccarat รับโบนัสแรกเข้าทันที และลุ้นรับโบนัสอีกมากมายกับโปรโมชั่นประจำเดือน สนใจเล่นบาคาร่ากับเราติดต่อเราได้ตลอด 24 ช.ม.</div>
-               </div>
+    </div>
+    <div class="container my-4">
+      <div class="module-border-wrap-condition">
+        <div class="module-border-condition">
+          <div class="row justify-content-center">
+            <div class="col-xl-12 col-md-12 col-12 text-center">
+              <div class="color_orange font22" style="font-weight:500;">
+                ALLBACCARAT
+              </div>
+              <div class="font22" style="font-weight:400;">บาคาร่าออนไลน์</div>
             </div>
-         </div>
-         <div class="row justify-content-center text-center p-3">
-            <div class="col align-self-center">
-               <router-link to="/"><img src="/logo/sexybaccarat.png" width="100%;" style="max-width:100px;"/></router-link>
+            <div class="col-xl-5 col-md-5 col-12">
+              <img src="/sexy.png" width="100%;" />
             </div>
-            <div class="col align-self-center">
-               <router-link to="/"><img src="/logo/dreamgaming.png" width="100%;" style="max-width:80px;"/></router-link>
+            <div class="col-xl-7 col-md-7 col-12 font14 mt-3">
+              เราเป็นผู้ให้บริการบาคาร่าออนไลน์แบบเรียลไทม์ ส่งตรงจากบ่อน Casino
+              ผู้เล่นสามารถดูไพ่ที่แจก ก่อนเริ่มเดิมพันได้ในเวลา 20-30 วินาที
+              Baccarat Online เป็นเกมส์ที่มีเสน่ห์ มีนักพนันเล่นกันเป็นจำนวนมาก
+              ผู้เล่นยังสามารถรับเงินรางวัลจากเกมส์ไปได้
+              ภายในเวลาไม่กี่วินาทีก่อนที่เกมส์การแข่งขันจะจบลงอีกด้วย
+              และผู้เล่นจะได้รับประสบการณ์ การเล่นคาสิโนออนไลน์ที่ปลอดภัย
+              โดยมีระบบตรวจสอบที่ได้รับการยอมรับในระดับสากล สมัครเล่นบาคาร่ากับ
+              Sexybaccarat รับโบนัสแรกเข้าทันที
+              และลุ้นรับโบนัสอีกมากมายกับโปรโมชั่นประจำเดือน
+              สนใจเล่นบาคาร่ากับเราติดต่อเราได้ตลอด 24 ช.ม.
             </div>
-            <div class="col align-self-center">
-               <router-link to="/"><img src="/logo/pretty-gaming.png" width="100%;" style="max-width:100px;"/></router-link>
-            </div>
-            <div class="col align-self-center">
-               <router-link to="/"><img src="/logo/ag-asia-gaming.png" width="100%;" style="max-width:100px;"/></router-link>
-            </div>
-            <div class="col align-self-center">
-               <router-link to="/"><img src="/logo/sagaming.png" width="100%;" style="max-width:100px;"/></router-link>
-            </div>
-         </div>
+          </div>
+        </div>
       </div>
-   </div>
+      <div class="row justify-content-center text-center p-3">
+        <div class="col align-self-center">
+          <router-link to="/"
+            ><img
+              src="/logo/sexybaccarat.png"
+              width="100%;"
+              style="max-width:100px;"
+          /></router-link>
+        </div>
+        <div class="col align-self-center">
+          <router-link to="/"
+            ><img
+              src="/logo/dreamgaming.png"
+              width="100%;"
+              style="max-width:80px;"
+          /></router-link>
+        </div>
+        <div class="col align-self-center">
+          <router-link to="/"
+            ><img
+              src="/logo/pretty-gaming.png"
+              width="100%;"
+              style="max-width:100px;"
+          /></router-link>
+        </div>
+        <div class="col align-self-center">
+          <router-link to="/"
+            ><img
+              src="/logo/ag-asia-gaming.png"
+              width="100%;"
+              style="max-width:100px;"
+          /></router-link>
+        </div>
+        <div class="col align-self-center">
+          <router-link to="/"
+            ><img
+              src="/logo/sagaming.png"
+              width="100%;"
+              style="max-width:100px;"
+          /></router-link>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {}
 </script>
 
 <style>
 .BG-linear {
-   background: linear-gradient(180deg, #3f0bc3 0%, #3f0bc3 0.01%, rgba(231, 2, 249, 0.48) 50.52%, rgba(252, 0, 85, 0) 100%);
+  background: linear-gradient(
+    180deg,
+    #3f0bc3 0%,
+    #3f0bc3 0.01%,
+    rgba(231, 2, 249, 0.48) 50.52%,
+    rgba(252, 0, 85, 0) 100%
+  );
 }
 .col {
-   padding-right: 5px;
-   padding-left: 5px;
+  padding-right: 5px;
+  padding-left: 5px;
 }
 .linear_gradient {
-   background: linear-gradient(90deg, rgba(2, 0, 36, 1) 0%, rgba(255, 128, 0, 1) 0%, rgba(255, 255, 255, 0) 100%);
-   height: 5px;
+  background: linear-gradient(
+    90deg,
+    rgba(2, 0, 36, 1) 0%,
+    rgba(255, 128, 0, 1) 0%,
+    rgba(255, 255, 255, 0) 100%
+  );
+  height: 5px;
 }
 .module-border-wrap-condition {
-   max-width: 100%;
-   position: relative;
-   background: linear-gradient(165deg, #3f0bc3, #c800ad);
-   padding: 4px;
-   border-radius: 6px !important;
+  max-width: 100%;
+  position: relative;
+  background: linear-gradient(165deg, #3f0bc3, #c800ad);
+  padding: 4px;
+  border-radius: 6px !important;
 }
 .module-border-condition {
-   background: #101010;
-   color: white;
-   padding: 20px 25px 25px 25px;
-   border-radius: 6px !important;
+  background: #101010;
+  color: white;
+  padding: 20px 25px 25px 25px;
+  border-radius: 6px !important;
 }
 </style>
